af7334b034d165dd6063ad96fa44ffe0b7e49e38
mhshrabon665-sys <mhshrabon665@gmail.com>
2026-03-25 22:48:57 +0600

    Delete app directory

--- app/portfolio/page.js
+++
@@ -1,8 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-
-const Builder = dynamic(() => import('@/components/Builder'), { ssr: false });
-
-export default function PortfolioPage() {
-  return <Builder />;
-}
